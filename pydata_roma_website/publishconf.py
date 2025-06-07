ab0d7962b7550fa39a67bda0b6aac56114b9a2cb
EgonFerri <egon.ferri@gmail.com>
2025-06-07 14:11:31 +0200

    fix: broken url in pelicanconf

--- pydata_roma_website/publishconf.py
+++ pydata_roma_website/publishconf.py
@@ -1,7 +1,5 @@
 import sys, pathlib
 sys.path.insert(0, str(pathlib.Path(__file__).parent))
 from pelicanconf import *
-# Public URL of the website when published.  The project is served from the
-# ``website/`` sub-folder of the GitHub Pages site, so include that path here.
-SITEURL = "https://PyDataRomaCapitale.github.io/website"
+SITEURL = "https://PyDataRomaCapitale.github.io"
 RELATIVE_URLS = False
