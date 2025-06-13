536df4a94d905c0f48c9fb5a83915a5b1ab222e5
EgonFerri <egon.ferri@gmail.com>
2025-06-13 16:45:22 +0200

    chore: update url in publishconf

--- pydata_roma_website/publishconf.py
+++ pydata_roma_website/publishconf.py
@@ -1,5 +1,5 @@
 import sys, pathlib
 sys.path.insert(0, str(pathlib.Path(__file__).parent))
 from pelicanconf import *
-SITEURL = "https://PyDataRomaCapitale.github.io"
+SITEURL = "https://pydataroma.python.it/"
 RELATIVE_URLS = False
