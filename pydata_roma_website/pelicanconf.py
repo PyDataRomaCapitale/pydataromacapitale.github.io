8e468e2b5758e072496303c67d42516cad256930
Carlo Antonio Venditti <c.a.venditti@pm.me>
2025-11-22 00:49:26 +0100

    feat: New memebers, WIP

--- pydata_roma_website/pelicanconf.py
+++ pydata_roma_website/pelicanconf.py
@@ -20,25 +20,53 @@ MENUITEMS = [
 
 BOARD_MEMBERS = [
     {
-        "name": "Egon Ferri",
-        "photo": "https://media.licdn.com/dms/image/v2/D4D03AQFsaWSiEKPu6w/profile-displayphoto-shrink_800_800/B4DZdLGfZoH4Ag-/0/1749311683005?e=1755129600&v=beta&t=UtH8tmsltsf-vpSTAm49JbCho1fmUFxS7bBSMuQF3zI",
-        "linkedin": "https://www.linkedin.com/in/egon-ferri/",
-        "role": "Organizer",
-        "tagline": "CV Engineer @ Immobiliare.it",
+        "name": "Egon",
+        "photo": f"{SITEURL}/images/people/Egon.jpg",
+        "linkedin": "#",
+        "role": "Board Member",
+        "tagline": "Update description",
     },
     {
-        "name": "Carmela Salandria",
-        "photo": "https://media.licdn.com/dms/image/v2/D4E03AQEiqgoMnc6EGA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1669741594156?e=1752710400&v=beta&t=8OLrev9-axaKDFdyFKkeFWqdfSrKP-bxyXAObxD6x_c",
-        "linkedin": "https://www.linkedin.com/in/carmela-salandr%C3%ACa-843162178/",
-        "role": "Organizer",
-        "tagline": "Data Engineer @ ELIS Innovation Hub ",
-    },
-    {
-        "name": "Vincenzo Ventriglia",
-        "photo": "https://media.licdn.com/dms/image/v2/D4D03AQG7zQIGoOAtNg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1689435365433?e=1752710400&v=beta&t=fgVFRkk8lN3a7jJpikA-eooKhvQFgOqZJWrh7lkrpsE",
+        "name": "Vincenzo",
+        "photo": f"{SITEURL}/images/people/Vincenzo.jpg",
         "linkedin": "https://www.linkedin.com/in/vincenzoventriglia/",
         "role": "Organizer",
         "tagline": "ML Engineer & Data Scientist @ INGV",
+    },
+    {
+        "name": "Carmela",
+        "photo": f"{SITEURL}/images/people/Carmela.jpg",
+        "linkedin": "#",
+        "role": "Board Member",
+        "tagline": "Update description",
+    },
+    {
+        "name": "Francesco",
+        "photo": f"{SITEURL}/images/people/Francesco.jpg",
+        "linkedin": "#",
+        "role": "Board Member",
+        "tagline": "Update description",
+    },
+    {
+        "name": "Carlo",
+        "photo": f"{SITEURL}/images/people/Carlo.jpg",
+        "linkedin": "#",
+        "role": "Board Member",
+        "tagline": "Update description",
+    },
+    {
+        "name": "Marco",
+        "photo": f"{SITEURL}/images/people/Marco.jpg",
+        "linkedin": "#",
+        "role": "Board Member",
+        "tagline": "Update description",
+    },
+    {
+        "name": "Vanessa",
+        "photo": f"{SITEURL}/images/people/Vanessa.jpg",
+        "linkedin": "#",
+        "role": "Board Member",
+        "tagline": "Update description",
     },
 ]
 
